edf22d1eff1e82f88fabd63854b199fa5364d7e5
sabbirtlp <sabbirtlp@gmail.com>
2026-05-01 15:47:37 +0600

    fix: add missing toast import in EditProductClient to fix build failure

--- src/app/admin/products/[id]/edit/EditProductClient.tsx
+++ src/app/admin/products/[id]/edit/EditProductClient.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../../../admin.module.css";
